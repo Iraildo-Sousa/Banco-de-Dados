4d3d5518e2d5f1b064888cb843f8447c3575cd29
Nodelay <121840258+Iraildo-Sousa@users.noreply.github.com>
2025-03-14 17:47:11 -0300

    Add files via upload

---
+++ Exercicios/08_Exercicio_SQL.sql
@@ -0,0 +1,29 @@
+-- SELECT * FROM relatorioEmpreiteira;
+
+/* O cliente solicitou um relatório de quem está trabalhando no Canteiro de Obra e qual a empresa que é responsável e qual a data que iniciou a obra e qual a previsão de dias para a obra.
+
+Nome dos Funcionários Canteiro da Obra
+Função dos Funcionários 
+Nome da Empresa Responsável
+Data Inicio da Obra
+Previsão da Obra
+Valor da Obra */
+
+SELECT * FROM canteiroObra;
+SELECT * FROM obra;
+SELECT * FROM projeto;
+SELECT * FROM colaborador;
+SELECT * FROM empreiteira;
+SELECT * FROM pessoa
+
+;SELECT 
+
+O.dataInicio, O.previsao, O.valor, PE.nomeFantasia, C.funcao, PC.nome AS nomeFuncionario
+
+FROM canteiroObra CO
+
+INNER JOIN obra O ON O.idObra = CO.idObra
+INNER JOIN empreiteira E ON E.idEmpreiteira = O.idEmpreiteira
+INNER JOIN pessoa PE ON PE.idPessoa = E.idPessoa
+INNER JOIN colaborador C ON C.idColaborador = CO.idColaborador
+INNER JOIN pessoa PC ON PC.idPessoa = C.idPessoa
